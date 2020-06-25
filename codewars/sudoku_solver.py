6dabfd1348e0a196a22433aca4d7ae1177b5f473
Nikolay Yusev <n.yusev@bi.zone>
2020-06-25 09:47:58 +0700

    add all files

---
+++ codewars/sudoku_solver.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/5296bc77afba8baa690002d7/train/python
+
+def sudoku(puzzle):
+    """return the solved puzzle as a 2d array of 9 x 9"""
+    return puzzle
+
+
+puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+          [6, 0, 0, 1, 9, 5, 0, 0, 0],
+          [0, 9, 8, 0, 0, 0, 0, 6, 0],
+          [8, 0, 0, 0, 6, 0, 0, 0, 3],
+          [4, 0, 0, 8, 0, 3, 0, 0, 1],
+          [7, 0, 0, 0, 2, 0, 0, 0, 6],
+          [0, 6, 0, 0, 0, 0, 2, 8, 0],
+          [0, 0, 0, 4, 1, 9, 0, 0, 5],
+          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+print(sudoku(puzzle))
