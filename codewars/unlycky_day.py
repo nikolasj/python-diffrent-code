e0f96b2bf77703d6362ed02f2d1dad2fb9f0edca
Nikolay Yusev <yusev@justcontrol.it>
2020-07-26 13:00:31 +0300

    add new kata

--- codewars/unlycky_day.py
+++ codewars/unlycky_day.py
@@ -1,7 +1,6 @@
 # https://www.codewars.com/kata/56eb0be52caf798c630013c0/train/python
 from datetime import date
 
-
 def unlucky_days(year):
     return sum(date(year, m, 13).weekday() == 4 for m in range(1, 13))
 
